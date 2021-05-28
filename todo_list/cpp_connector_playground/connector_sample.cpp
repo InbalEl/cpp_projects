4edc9e1d62b9c1483d32f55f28b90a9b25effd00
InbalEl <inbalelcode@gmail.com>
2021-05-28 16:23:29 +0300

    cpp_connector

---
+++ todo_list/cpp_connector_playground/connector_sample.cpp
@@ -0,0 +1,13 @@
+/* Standard C++ headers */
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+/* MySQL Connector/C++ specific headers */
+#include <mysql_connection.h>
+
+#define DBHOST "tcp://127.0.0.1:3306"
+#define USER "root"
+#define PASSWORD "#Inbal88"
